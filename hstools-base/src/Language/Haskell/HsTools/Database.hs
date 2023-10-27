d2cd3369063e2b033f70e2995f1c49bfa0b09006
Boldizsar Nemeth <boldizsarn@sqreamtech.com>
2023-10-27 09:03:43 +0200

    Reformat queries in Database.hs

--- hstools-base/src/Language/Haskell/HsTools/Database.hs
+++ hstools-base/src/Language/Haskell/HsTools/Database.hs
@@ -31,11 +31,23 @@ isSignatureDef DefClassOpSignature = True
 isSignatureDef _ = False
 
 getCompiledTime :: DbConn -> FilePath -> IO (Maybe UTCTime)
-getCompiledTime conn filePath = fmap (fmap head . listToMaybe) $ query conn "SELECT compiledTime FROM modules WHERE filePath = ?" (Only filePath)
+getCompiledTime conn filePath = fmap (fmap head . listToMaybe) $ query conn
+  [sql|
+    SELECT compiledTime
+    FROM modules
+    WHERE filePath = ?
+  |]
+  (Only filePath)
 
 getModuleIdLoadingState :: DbConn -> FilePath -> IO (Maybe (Int, LoadingState))
 getModuleIdLoadingState conn filePath = do
-  res <- query conn "SELECT moduleId, loadingState FROM modules WHERE filePath = ?" (Only filePath)
+  res <- query conn
+    [sql| 
+      SELECT moduleId, loadingState
+      FROM modules
+      WHERE filePath = ?
+    |]
+    (Only filePath)
   return $ case res of
     [(moduleId, loadingState)] -> Just (moduleId, toEnum loadingState)
     [] -> Nothing
@@ -44,65 +56,166 @@ getModuleIdLoadingState conn filePath = do
 updateLoadingState :: DbConn -> Int -> LoadingState -> IO ()
 updateLoadingState conn moduleId newLoadingState =
   void $ execute conn
-    "UPDATE modules SET loadingState = ? WHERE moduleId = ?"
+    [sql|
+      UPDATE modules
+      SET loadingState = ?
+      WHERE moduleId = ?
+    |]
     (fromEnum newLoadingState, moduleId)
 
 updateFileDiffs :: DbConn -> FilePath -> UTCTime -> Maybe String -> IO ()
-updateFileDiffs conn filePath modifiedTime serializedDiff = void $
-  execute conn "UPDATE modules SET modifiedTime = ?, modifiedFileDiffs = ? WHERE filePath = ?" (modifiedTime, serializedDiff, filePath)
+updateFileDiffs conn filePath modifiedTime serializedDiff =
+  void $ execute conn
+    [sql|
+      UPDATE modules
+      SET modifiedTime = ?, modifiedFileDiffs = ?
+      WHERE filePath = ?
+    |]
+    (modifiedTime, serializedDiff, filePath)
 
 getAllModifiedFileDiffs :: DbConn -> IO [(FilePath, Maybe String, Maybe UTCTime)]
-getAllModifiedFileDiffs conn = query_ conn "SELECT filePath, modifiedFileDiffs, modifiedTime FROM modules"
+getAllModifiedFileDiffs conn = query_ conn
+  [sql| SELECT filePath, modifiedFileDiffs, modifiedTime FROM modules |]
 
 getCompiledSource :: DbConn -> FilePath -> IO (Maybe String)
 getCompiledSource conn filePath
-  = fmap (fmap head . listToMaybe) $ query conn "SELECT compiledSource FROM modules WHERE filePath = ? AND compiledSource IS NOT NULL" (Only filePath)
+  = fmap (fmap head . listToMaybe) $ query conn
+      [sql|
+        SELECT compiledSource
+        FROM modules
+        WHERE filePath = ?
+          AND compiledSource IS NOT NULL
+      |]
+      (Only filePath)
 
 getModifiedTimeAndFileDiffs :: DbConn -> FilePath -> IO (Maybe String, Maybe UTCTime)
 getModifiedTimeAndFileDiffs conn filePath =
-  fromMaybe (Nothing, Nothing) . listToMaybe <$> query conn "SELECT modifiedFileDiffs, modifiedTime FROM modules WHERE filePath = ?" (Only filePath)
+  fromMaybe (Nothing, Nothing) . listToMaybe <$> query conn
+    [sql|
+      SELECT modifiedFileDiffs, modifiedTime
+      FROM modules
+      WHERE filePath = ?
+    |]
+    (Only filePath)
 
 getModifiedFileDiffs :: DbConn -> FilePath -> IO (Maybe String)
 getModifiedFileDiffs conn filePath
-  = fmap (fmap head . listToMaybe) $ query conn "SELECT modifiedFileDiffs FROM modules WHERE filePath = ? AND modifiedFileDiffs IS NOT NULL" (Only filePath)
+  = fmap (fmap head . listToMaybe) $ query conn
+      [sql|
+        SELECT modifiedFileDiffs
+        FROM modules
+        WHERE filePath = ?
+          AND modifiedFileDiffs IS NOT NULL
+      |]
+      (Only filePath)
 
 insertModule :: DbConn -> FilePath -> UTCTime -> String -> String -> String -> IO Int
 insertModule conn fullPath roundedModificationTime moduleName unitId content
-  = head . head <$> query conn "INSERT INTO modules (filePath, compiledTime, moduleName, unitId, loadingState, compiledSource, modifiedTime, modifiedFileDiffs) VALUES (?, ?, ?, ?, 0, ?, null, null) RETURNING moduleId"
+  = head . head <$> query conn 
+      [sql|
+        INSERT INTO modules (filePath, compiledTime, moduleName, unitId, loadingState, compiledSource, modifiedTime, modifiedFileDiffs)
+        VALUES (?, ?, ?, ?, 0, ?, null, null)
+        RETURNING moduleId
+      |]
       (fullPath, roundedModificationTime, moduleName, unitId, content)
 
 getAstNodes :: DbConn -> Int -> IO [(Int, Int, Int, Int, Int)]
-getAstNodes conn moduleId = query conn "SELECT startRow, startColumn, endRow, endColumn, astId FROM ast WHERE module = ?" (Only moduleId)
+getAstNodes conn moduleId =
+  query conn
+    [sql|
+      SELECT startRow, startColumn, endRow, endColumn, astId
+      FROM ast
+      WHERE module = ?
+    |]
+    (Only moduleId)
 
 getAllNames :: DbConn -> IO [(Int, Int, String, Maybe String, Bool)]
-getAllNames conn = query_ conn "SELECT startRow, startColumn, name, type, isDefined FROM ast AS n JOIN names nn ON nn.astNode = n.astId LEFT JOIN types tt ON tt.typedName = nn.name AND tt.typeNamespace = nn.namespace ORDER BY startRow, startColumn"
+getAllNames conn = query_ conn
+  [sql|
+    SELECT startRow, startColumn, name, type, isDefined
+    FROM ast AS n
+    JOIN names nn
+      ON nn.astNode = n.astId
+    LEFT JOIN types tt
+      ON tt.typedName = nn.name
+      AND tt.typeNamespace = nn.namespace
+    ORDER BY startRow, startColumn
+  |]
 
 persistAst :: DbConn -> [(Int, Int, Int, Int, Int)] -> IO [Int]
-persistAst conn asts = fmap head <$> returning conn "INSERT INTO ast (module, startRow, startColumn, endRow, endColumn) VALUES (?, ?, ?, ?, ?) RETURNING astId" asts
+persistAst conn asts = fmap head <$> returning conn
+  [sql|
+    INSERT INTO ast (module, startRow, startColumn, endRow, endColumn)
+    VALUES (?, ?, ?, ?, ?)
+    RETURNING astId
+  |]
+  asts
 
 persistDefinitions :: DbConn -> [(Int, Int, DefinitionKind)] -> IO [Int]
-persistDefinitions conn definitions = fmap head <$> returning conn "INSERT INTO definitions (module, astNode, definitionKind) VALUES (?, ?, ?) RETURNING definitionId" (map (\(a,b,c) -> (a,b, fromEnum c)) definitions)
+persistDefinitions conn definitions = fmap head <$> returning conn
+  [sql|
+    INSERT INTO definitions (module, astNode, definitionKind)
+    VALUES (?, ?, ?)
+    RETURNING definitionId
+  |]
+  (map (\(a,b,c) -> (a,b, fromEnum c)) definitions)
 
 persistName :: DbConn -> [(Int, Int, String, Maybe Int, Bool, Maybe Int, Maybe Int, Maybe Int, Maybe Int)] -> IO ()
-persistName conn names = void $ executeMany conn "INSERT INTO names (module, astNode, name, namespace, isDefined, namedDefinitionRow, namedDefinitionColumn, namedDefinitionEndRow, namedDefinitionEndColumn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)" names
+persistName conn names = void $ executeMany conn
+  [sql|
+    INSERT INTO names (module, astNode, name, namespace, isDefined, namedDefinitionRow, namedDefinitionColumn, namedDefinitionEndRow, namedDefinitionEndColumn)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  |]
+  names
 
 persistTypes :: DbConn -> [(String, Maybe Int, String)] -> IO ()
-persistTypes conn types = void $ executeMany conn "INSERT INTO types (typedName, typeNamespace, type) VALUES (?, ?, ?)" types
+persistTypes conn types = void $ executeMany conn
+  [sql|
+    INSERT INTO types (typedName, typeNamespace, type)
+    VALUES (?, ?, ?)
+  |]
+  types
 
 persistTHRange' :: DbConn -> Int -> Int -> IO ()
-persistTHRange' conn mod astNode = void $ execute conn "INSERT INTO thRanges (module, astNode) VALUES (?, ?)" (mod, astNode)
+persistTHRange' conn mod astNode = void $ execute conn
+  [sql|
+    INSERT INTO thRanges (module, astNode)
+    VALUES (?, ?)
+  |]
+  (mod, astNode)
 
 persistComments :: DbConn -> [(Int, Int, String)] -> IO ()
-persistComments conn comments = void $ executeMany conn "INSERT INTO comments (module, targetDefinition, commentText) VALUES (?, ?, ?)" comments
+persistComments conn comments = void $ executeMany conn
+  [sql|
+    INSERT INTO comments (module, targetDefinition, commentText)
+    VALUES (?, ?, ?)
+  |]
+  comments
 
 getAllComments :: DbConn -> IO [(Int, Int, String)]
-getAllComments conn = query_ conn "SELECT startRow, startColumn, commentText FROM ast a JOIN comments c ON c.targetDefinition = a.astId ORDER BY startRow, startColumn"
+getAllComments conn = query_ conn
+  [sql|
+    SELECT startRow, startColumn, commentText
+    FROM ast a
+    JOIN comments c
+      ON c.targetDefinition = a.astId
+    ORDER BY startRow, startColumn
+  |]
 
 persistMain :: DbConn -> Int -> String -> IO ()
-persistMain conn mod nm = void $ execute conn "INSERT INTO mains (module, name) VALUES (?, ?)" (mod, nm)
+persistMain conn mod nm = void $ execute conn
+  [sql|
+    INSERT INTO mains (module, name)
+    VALUES (?, ?)
+  |]
+  (mod, nm)
 
 getAllMains :: DbConn -> IO [[String]] -- not sure why this is list of strings
-getAllMains conn = query_ conn "SELECT name FROM mains"
+getAllMains conn = query_ conn
+  [sql|
+    SELECT name
+    FROM mains
+  |]
 
 getAllDefinitions :: DbConn -> IO [(DefinitionKind, Maybe String, Int, Int, Int, Int)]
 getAllDefinitions conn = fmap (\(k,n,a,b,c,d) -> (toEnum k,n,a,b,c,d)) <$> query_ conn 
@@ -120,38 +233,63 @@ getAllDefinitions conn = fmap (\(k,n,a,b,c,d) -> (toEnum k,n,a,b,c,d)) <$> query
   |]
 
 getTHRanges :: DbConn -> Int -> IO [(Int, Int, Int, Int)]
-getTHRanges conn moduleId
-  = query conn [sql|
+getTHRanges conn moduleId = query conn 
+  [sql|
     SELECT startRow, startColumn, endRow, endColumn
-      FROM thRanges t JOIN ast a ON t.astNode = a.astId
-      WHERE a.module = ?
-    |] (Only moduleId)
+    FROM thRanges t
+    JOIN ast a
+      ON t.astNode = a.astId
+    WHERE a.module = ?
+  |]
+  (Only moduleId)
 
 getAstId :: DbConn -> Int -> Int -> Int -> Int -> Int -> IO Int
-getAstId conn moduleId startRow startCol endRow endCol
-  = head . head <$>
-      query conn "SELECT astId FROM ast WHERE module = ? AND startRow = ? AND startColumn = ? AND endRow = ? AND endColumn = ?"
-        (moduleId, startRow, startCol, endRow, endCol)
+getAstId conn moduleId startRow startCol endRow endCol = head . head <$> query conn 
+  [sql|
+    SELECT astId
+    FROM ast
+    WHERE module = ?
+      AND startRow = ?
+      AND startColumn = ?
+      AND endRow = ?
+      AND endColumn = ?
+  |]
+  (moduleId, startRow, startCol, endRow, endCol)
 
 insertAstId :: DbConn -> Int -> Int -> Int -> Int -> Int -> IO Int
-insertAstId conn moduleId startRow startCol endRow endCol
-  = head . head <$>
-      returning conn "INSERT INTO ast (module, startRow, startColumn, endRow, endColumn) VALUES (?, ?, ?, ?, ?) RETURNING astId"
-        [(moduleId, startRow, startCol, endRow, endCol)]
+insertAstId conn moduleId startRow startCol endRow endCol = head . head <$> returning conn 
+  [sql|
+    INSERT INTO ast (module, startRow, startColumn, endRow, endColumn)
+    VALUES (?, ?, ?, ?, ?)
+    RETURNING astId
+  |]
+  [(moduleId, startRow, startCol, endRow, endCol)]
 
 getMatchingNames :: DbConn -> FilePath -> Int -> Int -> Maybe Bool -> IO [(String, Int, Int, Int, Int)]
-getMatchingNames conn file row col onlyDefinitions
-  = query conn
-      ([sql|
-        SELECT dm.filePath, d.startRow, d.startColumn, d.endRow, d.endColumn
-          FROM ast AS n JOIN names nn ON nn.astNode = n.astId
-            JOIN names AS dn ON nn.name = dn.name AND nn.namespace = dn.namespace
-            JOIN ast AS d ON d.astId = dn.astNode
-            JOIN modules nm ON n.module = nm.moduleId
-            JOIN modules dm ON d.module = dm.moduleId
-          WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?
-      |] `mappend` (fromString $ maybe "" ((" AND dn.isDefined = " ++) . show) onlyDefinitions))
-      (file, row, row, col, col)
+getMatchingNames conn file row col onlyDefinitions = query conn queryWithIsDefined (file, row, row, col, col)
+  where
+    queryWithoutIsDefined = 
+      [sql|
+          SELECT dm.filePath, d.startRow, d.startColumn, d.endRow, d.endColumn
+            FROM ast AS n
+              JOIN names nn
+                ON nn.astNode = n.astId
+              JOIN names AS dn
+                ON nn.name = dn.name
+                  AND nn.namespace = dn.namespace
+              JOIN ast AS d
+                ON d.astId = dn.astNode
+              JOIN modules nm
+                ON n.module = nm.moduleId
+              JOIN modules dm
+                ON d.module = dm.moduleId
+            WHERE nm.filePath = ?
+              AND n.startRow <= ?
+              AND n.endRow >= ?
+              AND n.startColumn <= ?
+              AND n.endColumn >= ?
+        |]
+    queryWithIsDefined = queryWithoutIsDefined `mappend` (fromString $ maybe "" ((" AND dn.isDefined = " ++) . show) onlyDefinitions)
 
 getHoverInfo :: DbConn -> FilePath -> Int -> Int -> IO [(Maybe String, Bool, String, Int, Int, Int, Int, Maybe String)]
 getHoverInfo conn file row col =
@@ -160,46 +298,78 @@ getHoverInfo conn file row col =
     [sql| 
       SELECT tn.type, nn.isDefined, nn.name, n.startRow, n.startColumn, n.endRow, n.endColumn, c.commentText
         FROM ast n
-        JOIN names nn ON nn.astNode = n.astId
-        JOIN modules nm ON n.module = nm.moduleId
-        LEFT JOIN types tn ON nn.name = tn.typedName AND nn.namespace = tn.typeNamespace
-        LEFT JOIN names dn ON nn.name = dn.name AND dn.isDefined = true
-        LEFT JOIN ast da ON dn.namedDefinitionRow = da.startRow AND dn.namedDefinitionColumn = da.startColumn 
-          AND dn.namedDefinitionEndRow = da.endRow AND dn.namedDefinitionEndColumn = da.endColumn
-        LEFT JOIN definitions d ON d.astNode = da.astId
-        LEFT JOIN comments c ON c.targetDefinition = d.definitionId
-        WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?
+        JOIN names nn
+          ON nn.astNode = n.astId
+        JOIN modules nm
+          ON n.module = nm.moduleId
+        LEFT JOIN types tn
+          ON nn.name = tn.typedName
+            AND nn.namespace = tn.typeNamespace
+        LEFT JOIN names dn
+          ON nn.name = dn.name
+            AND dn.isDefined = true
+        LEFT JOIN ast da
+          ON dn.namedDefinitionRow = da.startRow
+            AND dn.namedDefinitionColumn = da.startColumn 
+            AND dn.namedDefinitionEndRow = da.endRow
+            AND dn.namedDefinitionEndColumn = da.endColumn
+        LEFT JOIN definitions d
+          ON d.astNode = da.astId
+        LEFT JOIN comments c
+          ON c.targetDefinition = d.definitionId
+        WHERE nm.filePath = ?
+          AND n.startRow <= ?
+          AND n.endRow >= ?
+          AND n.startColumn <= ?
+          AND n.endColumn >= ?
     |]
     (file, row, row, col, col)
 
 logErrorMessage :: Connection -> UTCTime -> String -> String -> IO ()
-logErrorMessage conn time context message
-  = void $ PLSQL.execute conn "INSERT INTO errorLogs (time, context, message) VALUES (?, ?, ?)" (time, context, message)
+logErrorMessage conn time context message = void $ PLSQL.execute conn
+  [sql|
+    INSERT INTO errorLogs (time, context, message)
+    VALUES (?, ?, ?)
+  |]
+  (time, context, message)
 
 getErrors :: DbConn -> IO [(String, String)]
-getErrors conn = query_ conn "SELECT context, message FROM errorLogs"
+getErrors conn = query_ conn
+  [sql|
+    SELECT context, message
+    FROM errorLogs
+  |]
 
 listenToModuleClean :: DbConn -> IO ()
 listenToModuleClean conn = void $ execute_ conn "LISTEN module_clean"
 
 cleanModuleFromDB :: DbConn -> FilePath -> IO ()
 cleanModuleFromDB conn filePath = do
-  moduleIds <- query conn "SELECT moduleId FROM modules WHERE filePath = ?" [filePath]
+  moduleIds <- query conn 
+    [sql|
+      SELECT moduleId
+      FROM modules
+      WHERE filePath = ?
+    |]
+    [filePath]
   forM_ moduleIds $ \[moduleId] -> cleanRelatedData conn moduleId
 
 cleanModulesFromDB :: DbConn -> FilePath -> IO ()
 cleanModulesFromDB conn filePath = do
-  moduleIds <- query conn "SELECT moduleId FROM modules WHERE filePath LIKE '%' || ? || '%'" [filePath]
+  moduleIds <- query conn 
+    [sql|
+      SELECT moduleId
+      FROM modules
+      WHERE filePath LIKE '%' || ? || '%'
+    |]
+    [filePath]
   forM_ moduleIds $ \[moduleId] -> cleanRelatedData conn moduleId
 
 cleanRelatedData :: DbConn -> Int -> IO ()
 cleanRelatedData conn moduleId = void $ do
-  execute conn "DELETE FROM mains WHERE module = ?" [moduleId]
-  execute conn "DELETE FROM definitions WHERE module = ?" [moduleId]
-  execute conn "DELETE FROM comments WHERE module = ?" [moduleId]
-  execute conn "DELETE FROM names WHERE module = ?" [moduleId]
-  execute conn "DELETE FROM thRanges WHERE module = ?" [moduleId]
-  execute conn "DELETE FROM ast WHERE module = ?" [moduleId]
+  mapM_
+    (\table -> execute conn (fromString $ "DELETE FROM " ++ table ++ " WHERE module = ?") [moduleId])
+    ["mains", "definitions", "comments", "names", "thRanges", "ast"]
   execute conn "DELETE FROM modules WHERE moduleId = ?" [moduleId]
   execute_ conn "DELETE FROM types WHERE NOT EXISTS (SELECT 1 FROM names WHERE typedName = name AND typeNamespace = namespace)"
 
